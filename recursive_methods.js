ec4fc3d6e1c697bbded155fd56f15517f6fc3885
name": null <email": null>
2017-07-27 11:19:10 +0700

    tama

--- recursive_methods.js
+++ recursive_methods.js
@@ -1,5 +1,12 @@
 let choose_team = (n, k) => {
   //write your code here
+  if (k === 1)
+    return n
+  else if (n === k) {
+    return 1
+  } else {
+    return choose_team(n - 1, k - 1) + choose_team(n - 1, k)
+  }
 }
 
 console.log(choose_team(6, 2));
